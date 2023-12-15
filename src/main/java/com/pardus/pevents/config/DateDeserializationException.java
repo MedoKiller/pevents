48b147916d4cd1d1a32b5bc7145a44d9d9acbbc6
Dino <dinomed123@gmail.com>
2023-12-15 16:53:03 +0100

    jackson config for timestamps

---
+++ src/main/java/com/pardus/pevents/config/DateDeserializationException.java
@@ -0,0 +1,8 @@
+package com.pardus.pevents.config;
+
+public class DateDeserializationException extends RuntimeException {
+
+  public DateDeserializationException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
